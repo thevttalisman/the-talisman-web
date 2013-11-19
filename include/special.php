49d153e24a218d0c06d0f7f33a619f5e728909ae
thevttalisman <tech@thetalisman.ca>
2013-11-19 09:44:45 -0800

    Added the ability to override the month

--- include/special.php
+++ include/special.php
@@ -1,5 +1,10 @@
 <?php
 date_default_timezone_set("Canada/Pacific");
-if(date("n") == 12) echo "<div class=\"xmas-lights\" style=\"background-image: url(pics/xmas-lights/anim-" . rand(1, 3) . ".gif);\"></div>"
+if(getMonth() == 12) echo "<div class=\"xmas-lights\" style=\"background-image: url(pics/xmas-lights/anim-" . rand(1, 3) . ".gif);\"></div>"
 
+
+function getMonth() {
+  if(isset($_GET['override_month'])) return $_GET['override_month'];
+  else return date("n");
+}
 ?>
