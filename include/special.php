c2e8d6e56989b999a5ac1fc58f4292ab4beead4e
ssttevee <stevyo99@yahoo.ca>
2013-11-18 20:55:13 -0800

    added date_default_timezone_set

--- include/special.php
+++ include/special.php
@@ -1,5 +1,5 @@
 <?php
-
+date_default_timezone_set("Canada/Pacific");
 if(date("n") == 12) echo "<div class=\"xmas-lights\" style=\"background-image: url(pics/xmas-lights/anim-" . rand(1, 3) . ".gif);\"></div>"
 
 ?>
